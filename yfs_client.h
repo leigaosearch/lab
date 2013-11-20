b89cad5170fc69b38055996e7cc767806f3d168d
Lei <gaoleilucky@gmail.com>
2013-11-20 12:04:10 +0800

    solution 2

--- yfs_client.h
+++ yfs_client.h
@@ -6,8 +6,6 @@
 #include "extent_client.h"
 #include <vector>
 
-#include "lock_protocol.h"
-#include "lock_client.h"
 
 class yfs_client {
   extent_client *ec;
@@ -36,6 +34,7 @@ class yfs_client {
  private:
   static std::string filename(inum);
   static inum n2i(std::string);
+  static inum new_inum(bool isfile);
  public:
 
   yfs_client(std::string, std::string);
@@ -45,6 +44,14 @@ class yfs_client {
 
   int getfile(inum, fileinfo &);
   int getdir(inum, dirinfo &);
+  int setattr(inum, fileinfo &);
+  int createfile(inum, const char *, inum &);
+  int createdir(inum, const char *, inum &);
+  int createroot(inum, const char *);
+  int lookup(inum, const char *, inum &);
+  int readdir(inum, std::vector<dirent> &);
+  int read(inum, off_t off, size_t size, std::string &);
+  int write(inum, const char *, off_t off, size_t size);
 };
 
 #endif 
